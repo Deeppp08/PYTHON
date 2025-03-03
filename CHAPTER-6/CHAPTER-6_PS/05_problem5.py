e84f9f6153a9665c0e4d75b778aac6893d11e704
Deeppp08 <deeppp4002@gmail.com>
2025-03-03 22:54:38 +0530

    we also use IN keyword in list

---
+++ CHAPTER-6/CHAPTER-6_PS/05_problem5.py
@@ -0,0 +1,8 @@
+# Write a program which finds out whether a given name is present in a list or not.
+
+l = ["Deep", "Aditi", "Kavya", "Anshu", "Sejal", "Divyanshu"]
+name = input("Enter your name :- ")
+if(name in l):
+    print("Your name is in the list")
+else:
+    print("Your name is not in the list")
