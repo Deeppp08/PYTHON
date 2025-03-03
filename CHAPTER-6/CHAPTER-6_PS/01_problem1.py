7433eb7042b00097540efed5cec8bf78f485de5a
Deeppp08 <deeppp4002@gmail.com>
2025-03-03 23:24:05 +0530

    Update 01_problem1.py

---
+++ CHAPTER-6/CHAPTER-6_PS/01_problem1.py
@@ -0,0 +1,8 @@
+# Write a program to find out whether a given post is talking about “Deep” or not.
+
+post = input("Enter the post :- ")
+
+if("Deep".lower() in post.lower()):
+    print("This post is talking about Deep")
+else:
+    print("This post is not talking about Deep")
